d44b4cc109f69c1809c4a765738e8c0e0136a50f
guitarz777 <guitarz777@gmail.com>
2018-09-14 22:59:01 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,6 +59,14 @@ class TicTacToe
     return count
   end
   
-  
+  def current_player(board)
+    if turn_count(board) % 2 == 0 
+      return "X"
+    else
+      return "O"
+    end
+  end
 
+  
+  
 end
