82f11a566c87f231ed4a32b627d40760fbcae3a4
guitarz777 <guitarz777@gmail.com>
2018-09-14 22:58:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,6 +49,14 @@ class TicTacToe
     end
   end
   
-  
+  def turn_count(@board)
+    count = 0
+    @board.each do |i|
+      if i == "X" || i == "O"
+        count += 1
+      end
+    end
+    return count
+  end
 
 end
